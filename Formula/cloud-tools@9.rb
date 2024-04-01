7830ebbfa9d11b8869b15a220c445b26cfea788b
Cloperators <clops@jamf.com>
2024-04-01 18:22:22 +0000

    Another Cloud release 9/merge

---
+++ Formula/cloud-tools@9.rb
@@ -0,0 +1,65 @@
+
+require 'json'
+
+class CloudToolsAT9 < Formula
+    include Language::Python::Virtualenv
+    desc "Cloud Operations Tools Homebrew Dev"
+    homepage "https://github.com/jamf/cloud-ops-tools"
+
+    release = JSON.parse(File.open(File.expand_path('../../cloud/release-dev.json', __FILE__)).read)
+    version release['version']
+    license "MIT"
+
+    url "https://github.com/jamf/cloud-ops-tools/releases/download/#{version}/cloud-tools-#{version}.tar.gz"
+    sha256 release['sha256']
+    
+    depends_on "python@3.12"
+    depends_on "pkg-config" => :build
+    depends_on "rust" => :build
+    depends_on "certifi"
+    depends_on "cryptography"
+    depends_on "mozjpeg"
+    depends_on "cmake" => :build
+    depends_on "libxml2" => :build
+    depends_on "libxslt" => :build
+
+    
+    resource "certifi" do
+        url "https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2024.2.2.tar.gz"
+        sha256 "0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f"
+    end
+    
+    resource "charset-normalizer" do
+        url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
+        sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
+    end
+    
+    resource "idna" do
+        url "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz"
+        sha256 "9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca"
+    end
+    
+    resource "requests" do
+        url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
+        sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
+    end
+    
+    resource "urllib3" do
+        url "https://files.pythonhosted.org/packages/7a/50/7fd50a27caa0652cd4caf224aa87741ea41d3265ad13f010886167cfcc79/urllib3-2.2.1.tar.gz"
+        sha256 "d0570876c61ab9e520d776c38acbbb5b05a776d3f9ff98a5c8fd5162a444cf19"
+    end
+    
+
+    def python3
+        "python3.12"
+    end
+  
+    def install
+        venv = virtualenv_create(libexec, python3)
+        venv.pip_install resources
+        venv.pip_install_and_link buildpath
+    end
+  
+    test do
+    end
+  end
